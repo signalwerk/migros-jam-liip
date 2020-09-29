cfde98e30f3c9bd7e259445122ae04186444abba
signalwerk <sh@signalwerk.ch>
2020-09-29 11:06:20 +0200

    Updates

---
+++ _nuxt/030da99.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{262:function(n,t,e){"use strict";e.r(t);var l=e(12),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("Nuxt")}),[],!1,null,null,null);t.default=component.exports}}]);
